3963be069eb588e4739812fe5d92dc481254c6eb
Jeremy Bedolla <jjbedolla@gmail.com>
2019-11-19 02:44:14 +0000

    updated header

--- simpleshell.h
+++ simpleshell.h
@@ -1,6 +1,7 @@
 #ifndef SIMPLESHELL_H
 #define SIMPLESHELL_H
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,6 +9,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+char *_strdup(char *str);
+int _strncmp(char *s1, char *s2, int n);
+char getenviron(char *var);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
