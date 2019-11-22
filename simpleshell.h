bfdba421a4d1522373e15523ef0ee8f070f1b353
Jeremy Bedolla <jjbedolla@gmail.com>
2019-11-22 19:22:58 +0000

    changing returntype

--- simpleshell.h
+++ simpleshell.h
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
 char *_memcpy(char *dest, char *src, unsigned int n);
-int getline_helper(char *line);
+char * getline_helper(char *line);
 int exit_helper(char **args);
 
 #endif /* SIMPLESHELL_H */
