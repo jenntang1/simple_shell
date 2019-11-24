53200992247fb8ec736c1a5feba9047ba6206a01
jenntang1 <1039@holbertonschool.com>
2019-11-24 23:13:19 +0000

    Removed execution to another file

--- shell.c
+++ shell.c
@@ -11,42 +11,9 @@ int main(int argc, char **argv, char **env)
 {
 	pid_t child, pid;
 	int stat;
-	char *prompt;
+	char *prompt = "jj$";
 
 	while (1)
 	{
-		prompt = getcwd(BUFSIZE, sizeof(BUFSIZE));
-		write(STDOUT_FILENO, BUFSIZE, prompt);
-		child = fork();
-		if (child == -1)
-		{
-			perror("Error");
-			exit(EXIT_FAILURE);
-		}
-		if (child == 0)
-		{
-			if (execve(argv[0], argv, NULL) == -1)
-			{
-				perror("Error");
-				exit(EXIT_FAILURE);
-				return (-1);
-			}
-			else
-				execve(argv[0], argv, NULL);
-		}
-		else
-		{
-			do {
-				pid = waitpid(child, &stat, WUNTRACED);
-				if (pid == -1)
-				{
-					perror("waitpid");
-					exit(EXIT_FAILURE);
-					return (-1);
-				}
-			} while (!WIFEXITED(stat));
-			exit(EXIT_SUCCESS);
-		}
+		write(STDOUT_FILENO, *prompt, 3);
 	}
-	return (EXIT_SUCCESS);
-}
