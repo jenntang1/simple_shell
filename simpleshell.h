e2b36536af6c805658346dc0381448876c739129
jenntang1 <1039@holbertonschool.com>
2019-11-24 23:14:28 +0000

    Updated to include more standard libraries

--- simpleshell.h
+++ simpleshell.h
@@ -3,27 +3,35 @@
 
 #define BUFSIZE 1024
 
+#include <dirent.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strdup(char *str);
 int _strncmp(char *s1, char *s2, int n);
-char *getenviron(char *var);
-void *_calloc(unsigned int nmemb, unsigned int size);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _strlen(char *s);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *getline_helper(char *line);
-int exit_helper(char **args);
+
 char *pathfinder(char *line);
+char *getenviron(char *var);
+
 char **_printenv(char **str);
+int exit_helper(char **args);
+
+int execute(char **argv);
 
 #endif /* SIMPLESHELL_H */
