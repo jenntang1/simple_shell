fe882dbed5ca193c356bc908e373149661beb0d5
jenntang1 <1039@holbertonschool.com>
2019-11-24 02:21:09 +0000

    Removed comment

--- AUTHORS.c
+++ AUTHORS.c
@@ -1,5 +1,4 @@
 # This file lists all individuals that contributed content to the repository.
-# For how it is generated, see `simple_shell.jjsh`.
 
 Jeremy Bedolla <1106@holbertonschool.com>
 Jennifer Tang <1039@holbertonschool.com>
