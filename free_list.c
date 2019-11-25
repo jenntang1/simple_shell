df6f30055977b6f0290505ba02ea182cc594117f
Jeremy Bedolla <jjbedolla@gmail.com>
2019-11-25 22:24:19 +0000

    free function

---
+++ free_list.c
@@ -0,0 +1,15 @@
+#include "simpleshell.h"
+
+/**
+ * free_list - frees linked list
+ * @head: head
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free(head->str);
+		free_list(head->next);
+		free(head);
+	}
+}
