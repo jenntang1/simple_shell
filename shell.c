42ee2ba04df1c0a556b065a79a11d8138515b749
Jeremy Bedolla <jjbedolla@gmail.com>
2019-11-25 22:21:48 +0000

    pushing to update partner

--- shell.c
+++ shell.c
@@ -1,5 +1,6 @@
 #include "simpleshell.h"
 
+char *shell = *argv[0];
 /**
  * main - function that invokes simple shell
  * @argc: number of arguments
@@ -7,12 +8,31 @@
  * @env: NULL terminated array of strings
  * Return: 0
  */
+
 int main(int argc, char **argv, char **env)
 {
-	char *prompt = "jj$";
+	char *line = NULL;
+	buff = 0;
+	int status = 0;
+
 
 	while (1)
 	{
-		write(STDOUT_FILENO, *prompt, 3);
+		if (isatty(STDIN_FILENO) == 1)
+		write(1, "jj$", 3);
+		if (isatty(STDIN_FILENO) == 0)
+		{
+			write(1,"\n", 1);
+		break;
+		}
+		status = getline(&line, &buff, stdin);
+		if (status == -1)
+		{
+			write(1,shell,strlen(shell));
+			write(1,": File name too long". 20);
+			continue;
+		}
+
+
 	}
 }
